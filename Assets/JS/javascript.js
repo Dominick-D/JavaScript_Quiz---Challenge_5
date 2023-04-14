3cbb0c8ea35121764a27734a6a342de8a1999dfe
Dominick-D <dominickdonn@gmail.com>
2023-04-13 22:09:00 -0400

    Bootstrap/Finishing Touches

--- Assets/JS/javascript.js
+++ Assets/JS/javascript.js
@@ -24,47 +24,37 @@ var questions = [
       choices: ["The current function", "The parent function", "The global object", "The object that owns the current function"],
       answer: "The object that owns the current function"
     }
-  ];
+  ];  console.log(questions[0]);
 
-  console.log(questions[0]);
 var quizContainer= document.getElementById('quiz');
 var resultContainer= document.getElementById('result');
 var highScoresContainer= document.getElementById('high-scores');
 var startButton= document.getElementById('start-button');
 var timerElement = document.getElementById("timer");
-timerElement.style.display = 'none';
-startButton.addEventListener('click', startQuiz);
-
+var timeLeft= 60;
 var currentQuestion= 0;
 var score= 0;
-var timeLeft= 120;
 let timerId;
 
+timerElement.style.display = 'none';
 highScoresContainer.style.display = "none";
+
+startButton.addEventListener('click', startQuiz);
 
 function startQuiz(){
     startButton.style.display= 'none';
     timerElement.style.display= 'block';
     quizContainer.style.display= 'block';
     displayQuestion();
-    //updateTimerDisplay();
     timerId= setInterval(updateTimer,1000);
 }
 
 function displayQuestion(){
-    /*var question = questions[currentQuestion];
-    var questions=question.choices.map(choice => `<li><button onclick="checkAnswer('${choice}')">${choice}</button></li>`).join('');
-    var questionHTML= `
-    <h2>${question.question}</h2>
-    <ul>`"'
-      ${questions} </ul>` ;
-      quizContainer.innerHTML = questionHTML; */
       document.getElementById("quiz").textContent = "";
       console.log(questions[currentQuestion]);
       var q = document.createElement("h4");
       q.textContent = questions[currentQuestion].question;
       document.getElementById("quiz").append(q);
-
       for(var i=0; i< questions[currentQuestion].choices.length;i++)
       {
         var btn = document.createElement("button");
@@ -94,10 +84,9 @@ function checkAnswer(){
 
 function updateTimer(){
     timeLeft--;
-    timerElement.textContent = `Time Left: ${timeLeft}`;
+    timerElement.textContent = `Time Left: ${timeLeft}!`;
     if(timeLeft<=0){endQuiz();}
 }
-
 
 function endQuiz(){
     clearInterval(timerId);
@@ -106,16 +95,31 @@ function endQuiz(){
     resultContainer.textContent=`Your final score is ${score}.`;
 }
 
-document.getElementById("submit").addEventListener("click", function(e){
-  e.preventDefault();
-  console.log(document.getElementById("initials").value);
-  var temp = {
-    initials: document.getElementById("initials").value,
+function saveScore(event){
+  event.preventDefault();
+  
+  var initialsInput = document.getElementById("initials").value;
+  var highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+  
+  highScores.push({
+    initials: initialsInput,
     score: score
-  };
-  var iniArr = JSON.parse(localStorage.getItem("scores")) || [];
+  });
+  
+  highScores.sort((a, b) => b.score - a.score);
+  localStorage.setItem("highScores", JSON.stringify(highScores));
+  
+  var scoresList = document.getElementById("score-list");
+  scoresList.innerHTML = "";
+  
+  for (var i = 0; i < highScores.length; i++) {
+    var scoreItem = document.createElement("li");
+    scoreItem.textContent = highScores[i].initials + " - " + highScores[i].score;
+    scoresList.appendChild(scoreItem);}
+};
 
-  iniArr.push(temp);
-  localStorage.setItem("scores", JSON.stringify(iniArr));
-})
-
+document.getElementById("submit").addEventListener("click", saveScore); 
+document.getElementById("clear-scores").addEventListener("click", function() {
+  localStorage.removeItem("highScores");
+  document.getElementById("high-scores-table").innerHTML = "";
+});
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# JavaScript_Quiz---Challenge_5
-Short 2 minute javascript quiz, Can save your score to your local storage!
+# Novice JavaScript Quiz
+---
+## Description
+
+This JavaScript Quiz has 5 questions and gives you a minute to answer. Becareful, if you get a question wrong it takes 10 seconds off the timer! I built this quiz so others can have fun, in the future I may add more questions to study!
+
+## Usage
+
+Click red Start Quiz button to start the questions/timer. at the end you get a rank on the leaderboard saved to your in your local storage saved with your initials.
+
+![alt text](assets/images/screenshot.png)
+
+
+## License
+
+MIT License
+
+Copyright (c) 2023 Dominick-D
+
+---
+
